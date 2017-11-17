9426ad13b41efbd42dce263e528a5bed0e979cd9
K1D77A <k1llfrenzy@Hotmail.co.uk>
2017-11-17 22:07:37 +0000

    Fixed some more stuff

--- cl-jaro.lisp
+++ cl-jaro.lisp
@@ -1,23 +1,17 @@
 ;;;;original is here https://gist.github.com/ronnieoverby/2aa19724199df4ec8af6
 
-(defun distance (str1 str2  threshold number-chars)
-  (- 1.0 (proximity str1 str2 threshold number-chars)))
 
-
-(defun proximity (str1 str2 threshold number-chars) 
+(defun jaro-distance (str1 str2)
+  (if (string= str1 str2)
+      1)
   (let* ((lenstr1 (length str1))
 	 (lenstr2 (length str2))
-	 (search-range (max 0 (1- (/ (max lenstr1 lenstr2) 2))))
-	 (matched1 (make-array lenstr1))
-	 (matched2 (make-array lenstr2))
+	 (search-range (floor(max 0 (1- (/ (max lenstr1 lenstr2) 2)))))
+	 (matched1 (make-array lenstr1 :element-type 'boolean :initial-element nil))
+	 (matched2 (make-array lenstr2 :element-type 'boolean :initial-element nil))
 	 (common 0)
-	 (half-transposed 0)
 	 (k 0)
-	 (transposed 0)
-	 (common-d 0)
-	 (weight 0)
-	 (max 0)
-	 (pos 0))
+	 (transposed 0))
     (loop
        :for i :from 0 :to (1- lenstr1)
        :for start := (max 0 (- i search-range))
@@ -26,41 +20,39 @@
 	      :for j :from start :to (1- end)
 	      :for str1i := (aref str1 i)
 	      :for str2i := (aref str2 j)
-	     ;; :do (print (aref matched2 j))
-	      ;;:do (print (equal str1i str2i))
-	      :when (or (not (aref matched2 j)) (equal  str1i str2i))
+	      :when (and (not (aref matched2 j)) (equal str1i str2i))
 	      :do (progn   (setf (aref matched1 i) t)
 			   (setf (aref matched2 j) t)
 			   (incf common)
 			   (loop-finish))))
-    (if (zerop common)
-	0)
-    (loop
-       :for i :from 0 :upto (1- lenstr1)
-       :for val := (aref matched1 i)
-       :unless (not val)
-         :do (loop
-		:while (not (aref matched2 k))
-		:do (incf k))
-       :unless (not val)
-         :if (not  (equal  (aref str1 i)(aref str2 k)))
-           :do (incf half-transposed)
-       :do (incf k))
-    (setf transposed (/ half-transposed 2))
-   ; (print common)
-    (setf common-d common)
-    (setf weight (/ (+ (/ common-d lenstr1)
-		       (/ common-d lenstr2)
-		       (/ (- common transposed)
-			  common-d))
-		    3.0))
-    (if (<= weight threshold)
-	weight)
-    (setf max (min number-chars (min (length str1) (length str2))))
-    (loop
-       :while (and (< pos max) (equal (aref str1 pos)(aref str2 pos)))
-       :do (incf pos))
-    (if (zerop pos)
-	weight
-	(* (+ weight 0.1) pos (- 1.0 weight)))))
-	
+    (cond ((zerop common)
+	   0)
+	  (t 
+	   (loop
+	      :for i :from 0 :upto (1- lenstr1)
+	      :for val := (aref matched1 i)
+	      :if (or (>= k (1- lenstr2)) (>= i (1- lenstr1)));needed to add a break out if
+	      :do (loop-finish);k or i go above the array index max.
+	      :unless (not val);if no matches in str1 then continue
+	      :do (loop
+		     :while (and (not (aref matched2  k)) (< k (1- lenstr2)));while there is no match
+		     :do (incf k));in matched2 incf k
+	      :unless (not val)
+	      :if (not  (equal  (aref str1 i)(aref str2 k)))
+	      :do (incf transposed)
+	      :do (incf k));incf transpositions   	
+	   (/ (+ (/ common lenstr1)
+		 (/ common lenstr2);returning the jaro distance
+		 (/ (- common (/ transposed 2))
+		    common))
+	      3.0)))))
+#|CL-USER> (jaro-distance "jellyfish" "smellyfish")
+
+0.89629626
+CL-USER> (jaro-distance "dixon" "dicksonx")
+0.76666665
+CL-USER> (jaro-distance "martha" "marhta")
+0.9444444
+These are the same results that can be found at https://rosettacode.org/wiki/Jaro_distance#C.2B.2B
+
+#|
