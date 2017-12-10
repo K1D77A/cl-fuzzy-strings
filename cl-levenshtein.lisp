beb3c8927c3a002418956ea29901bb3856ece09b
K1D77A <k1llfrenzy@Hotmail.co.uk>
2017-12-10 10:15:18 +0000

    Levenshtein added. ty whoever put it online

---
+++ cl-levenshtein.lisp
@@ -0,0 +1,33 @@
+;;;;Levenshtein distance in CL.
+;;;found
+;;;https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Common_Lisp 
+;;;all credit goes to whoever wrote that.
+;;;https://en.wikipedia.org/wiki/Levenshtein_distance
+;;;"a string metric for measuring the difference between two sequences."
+(defun levenshtein-distance (str1 str2)
+  "Calculates the Levenshtein distance between str1 and str2, returns an editing distance (int)."
+  (let ((n (length str1))
+	(m (length str2)))
+    ;; Check trivial cases
+    (cond ((= 0 n) (return-from levenshtein-distance m))
+	  ((= 0 m) (return-from levenshtein-distance n)))
+    (let ((col (make-array (1+ m) :element-type 'integer))
+	  (prev-col (make-array (1+ m) :element-type 'integer)))
+      ;; We need to store only two columns---the current one that
+      ;; is being built and the previous one
+      (dotimes (i (1+ m))
+	(setf (svref prev-col i) i))
+      ;; Loop across all chars of each string
+      (dotimes (i n)
+	(setf (svref col 0) (1+ i))
+	(dotimes (j m)
+	  (setf (svref col (1+ j))
+		(min (1+ (svref col j))
+		     (1+ (svref prev-col (1+ j)))
+		     (+ (svref prev-col j)
+			(if (char-equal (schar str1 i)
+					(schar str2 j))
+			    0
+			    1)))))
+	(rotatef col prev-col))
+      (svref prev-col m))))
